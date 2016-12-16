cdc047874e674617ab6b84ea0088af15c235d436
Thomas Stringer <tr.stringer@outlook.com>
2016-12-16 18:47:57 -0500

    add termcolor as required package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='dlclean',
     version='0.1.0',
     packages=['dlclean'],
+    install_requires=['termcolor'],
     entry_points={
         'console_scripts': [
             'dlclean = dlclean.__main__:main'
